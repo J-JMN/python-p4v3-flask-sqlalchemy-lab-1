b5e1be864d431ad178daa7ca4b0fdb21d7fb9055
Joseph <j.mburu.pro@gmail.com>
2025-06-12 14:00:08 +0300

    Completed Flask-SQLAlchemy Lab 1

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +19,39 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+
+# Route: Get earthquake by ID
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+
+    if quake:
+        response = make_response(jsonify(quake.to_dict()), 200)
+    else:
+        response = make_response(
+            jsonify({'message': f"Earthquake {id} not found."}), 404
+        )
+    
+    return response
+
+
+# Route: Get earthquakes by minimum magnitude
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quake_list = [quake.to_dict() for quake in quakes]
+
+    response = make_response(
+        jsonify({
+            "count": len(quake_list),
+            "quakes": quake_list
+        }),
+        200
+    )
+
+    return response
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
